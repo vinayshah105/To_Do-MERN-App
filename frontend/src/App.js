6d37d72c7a4bdbfefac15c876406fe417750b340
vinayshah105 <shahvinay1005.sv@gmail.com>
2023-07-15 11:14:55 -0400

    first commit

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Footer from './component/Footer/Footer';
+import Header from './component/Header/Header';
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
